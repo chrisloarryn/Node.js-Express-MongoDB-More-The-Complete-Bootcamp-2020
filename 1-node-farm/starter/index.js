86bfbb37efe2d34c836db042a4553bd5c07ef3ce
cristobalcontrerasepys <elementalreggae@gmail.com>
2020-03-23 11:08:00 -0300

    adding finish of section

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -5,6 +5,7 @@ const http = require('http');
 const url = require('url');
 const host = '127.0.0.1';
 const port = 8000;
+var replaceTemplate = require('./modules/replaceTemplate');
 
 // Blocking, synchronous way
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
@@ -35,21 +36,6 @@ const port = 8000;
 // Server
 
 ////////////////
-
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENT%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-
-    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    return output;
-
-};
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
@@ -67,12 +53,14 @@ const dataObj = JSON.parse(data);
  * @param  {} res
  */
 const server = http.createServer((req, res) => {
-    const {query, pathname} = url.parse(req.url, true);
+    const { query, pathname } = url.parse(req.url, true);
 
     // console.log(query);
     // console.log(pathname);
 
-    // Overview page
+    /**
+     * @param  {} pathname==='/'||pathname==='/overview'
+     */
     if (pathname === '/' || pathname === '/overview') {
 
         res.writeHead(200, { 'Content-type': 'text/html' });
@@ -80,14 +68,18 @@ const server = http.createServer((req, res) => {
         const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
         res.end(output);
 
-        // Product page
+        /**
+         * @param  {} pathname==='/product'
+         */
     } else if (pathname === '/product') {
         res.writeHead(200, { 'Content-type': 'text/html' });
         const product = dataObj[query.id];
         const output = replaceTemplate(tempProduct, product);
         res.end(output);
 
-        // API
+        /**
+         * @param  {} pathname==='/api'
+         */
     } else if (pathname === '/api') {
         res.writeHead(200, { 'Content-type': 'application/json' });
         res.end(data);
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Node.js-Express-MongoDB-More-The-Complete-Bootcamp-2020
 Node.js, Express, MongoDB &amp; More The Complete Bootcamp 2020
+
+## was only generated to test express generator.
+/foo
