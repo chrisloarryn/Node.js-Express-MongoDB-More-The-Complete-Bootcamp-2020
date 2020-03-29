65c19265e2c8c083097ea405dc756ad894d6c9eb
cristobalcontrerasepys <elementalreggae@gmail.com>
2020-03-29 19:54:10 -0300

    natours

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -3,13 +3,8 @@ const dotenv = require('dotenv')
 const app = require('./app')
 dotenv.config({ path: './config.env' })
 
-
 // console.log(process.env)
 const port = process.env.PORT || 3000
 app.listen(port, () => {
   console.log(`App running on port ${port}... 😊`)
 })
-
-
-const x = 32323;
-x = 333;
