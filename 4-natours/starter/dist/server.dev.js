d578cae7ea44f478e6c1641cd26b5b1301e5e41b
cristobalcontrerasepys <elementalreggae@gmail.com>
2020-03-31 00:21:17 -0300

    mongoose

--- 4-natours/starter/dist/server.dev.js
+++ 4-natours/starter/dist/server.dev.js
@@ -1,12 +1,49 @@
 "use strict";
 
 // Related to express
+var mongoose = require('mongoose');
+
 var dotenv = require('dotenv');
 
 var app = require('./app');
 
 dotenv.config({
   path: './config.env'
+}); // DB is to connect to atlas.
+
+var DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+mongoose.connect(DB || process.env.DATABASE_LOCAL, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+}).then(function (con) {
+  console.log("DB Connection Successfully \uD83D\uDE01");
+});
+var tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    "default": 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+});
+var Tour = mongoose.model('Tour', tourSchema);
+var testTour = new Tour({
+  name: 'The Park Camper2',
+  price: 997
+});
+testTour.save().then(function (doc) {
+  console.log(doc);
+})["catch"](function (err) {
+  console.log("ERROR \uD83D\uDCA5:", err.message);
 }); // console.log(process.env)
 
 var port = process.env.PORT || 3000;
--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,7 +1,55 @@
 // Related to express
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const app = require('./app')
 dotenv.config({ path: './config.env' })
+
+// DB is to connect to atlas.
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+)
+mongoose
+  .connect(DB || process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(con => {
+    console.log(`DB Connection Successfully 😁`)
+  })
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+})
+const Tour = mongoose.model('Tour', tourSchema)
+
+const testTour = new Tour({
+  name: 'The Park Camper2',
+  price: 997
+})
+
+testTour
+  .save()
+  .then(doc => {
+    console.log(doc)
+  })
+  .catch(err => {
+    console.log(`ERROR 💥:`, err.message)
+  })
 
 // console.log(process.env)
 const port = process.env.PORT || 3000
